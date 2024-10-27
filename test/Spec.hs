513dda22b9cf6dc471e0e3d2f90e95ee1e1be4cc
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-10-27 19:05:03 +0000

    added interesting tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-import Test.Tasty ( TestTree, defaultMain, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
 
 import Lib1 qualified
 import Lib2 qualified
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 main :: IO ()
 main = defaultMain tests
@@ -12,11 +12,29 @@ tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
-  ]
+unitTests =
+  testGroup
+    "Lib1 tests"
+    [ testCase "Empty Query" $
+        Lib2.parseQuery "" @?= Left "Error, command doesn't match any known query.",
+      testCase "Parsing case 1 - give a better name" $
+        Lib2.parseChangeTempoMelody "changeTempoMelody 1 +3" @?= Right (Lib2.ChangeTempoMelody 1 (Lib2.SmallInteger Lib2.Plus 3), ""),
+      testCase "Creating melody" $
+        Lib2.parseCreateMelody "createMelody 1 F2F4F8(A2((G16))B4) stop"
+          @?= Right
+            ( Lib2.CreateMelody
+                1
+                ( Lib2.CompoundMelody
+                    [ Lib2.SingleNote (Lib2.Note Lib2.F Lib2.Half),
+                      Lib2.SingleNote (Lib2.Note Lib2.F Lib2.Quarter),
+                      Lib2.SingleNote (Lib2.Note Lib2.F Lib2.Eighth),
+                      Lib2.CompoundMelody
+                        [ Lib2.SingleNote (Lib2.Note Lib2.A Lib2.Half),
+                          Lib2.CompoundMelody [Lib2.CompoundMelody [Lib2.SingleNote (Lib2.Note Lib2.G Lib2.Sixteenth)]],
+                          Lib2.SingleNote (Lib2.Note Lib2.B Lib2.Quarter)
+                        ]
+                    ]
+                ),
+              ""
+            )
+    ]
