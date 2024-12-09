89cec663d800b1e700c33b90ff8bec2689ed0331
Dovydas Girskas <dovydas.girskas@mif.stud.vu.lt>
2024-12-09 22:23:47 +0000

    tests work

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,46 +1,79 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-import Test.Tasty ( TestTree, defaultMain, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
-import Test.Tasty.QuickCheck as QC
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.List
-import Data.Ord
+module Main where
 
-import Lib1 qualified
 import Lib2 qualified
+import Lib3 qualified
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.QuickCheck as QC
 
 main :: IO ()
 main = defaultMain tests
 
+genPitch :: Gen Lib2.Pitch
+genPitch = QC.elements [Lib2.A, Lib2.B, Lib2.C, Lib2.D, Lib2.E, Lib2.F, Lib2.G]
+
+genDuration :: Gen Lib2.Duration
+genDuration = QC.elements [Lib2.Whole, Lib2.Half, Lib2.Quarter, Lib2.Eighth, Lib2.Sixteenth]
+
+genNote :: Gen Lib2.Note
+genNote = Lib2.Note <$> genPitch <*> genDuration
+
+-- generate Melodiiiiies
+genMelody :: Int -> Gen Lib2.Melody
+genMelody 0 = Lib2.CompoundMelody . (: []) . Lib2.SingleNote <$> genNote
+genMelody depth =
+  QC.oneof
+    [ Lib2.CompoundMelody . (: []) . Lib2.SingleNote <$> genNote,
+      Lib2.CompoundMelody <$> listOf1 (genMelody (depth - 1))
+    ]
+
+-- Generate CreateMelody queries
+genCreateMelodyQuery :: Gen Lib2.Query
+genCreateMelodyQuery = do
+  mid <- QC.choose (1, 9) -- melody ID range
+  melody <- genMelody 3 -- Limit recursion depth
+  return $ Lib2.CreateMelody mid melody
+
+-- Modified to ONLY generate Batch statements (nes taip rendering veikia all good)
+genBatchStatements :: Gen Lib3.Statements
+genBatchStatements = Lib3.Batch <$> listOf1 genCreateMelodyQuery
+
+-- Update Arbitrary instance to only generate Batches
+instance Arbitrary Lib3.Statements where
+  arbitrary = genBatchStatements
+
+-- Update property test to focus on Batch round-trip
+propertyTests :: TestTree
+propertyTests =
+  testGroup
+    "Property Tests"
+    [ testProperty "batch rendering and parsing preserves structure" $
+        \s -> case Lib3.parseStatements (Lib3.renderStatements s) of
+          Right (parsedStatements, "") -> s == parsedStatements
+          _ -> False
+    ]
+
 tests :: TestTree
-tests = testGroup "Tests" [unitTests, propertyTests]
+tests =
+  testGroup
+    "Melody State Tests"
+    [ unitTests,
+      propertyTests
+    ]
 
 unitTests :: TestTree
 unitTests =
   testGroup
-    "Lib1 tests"
-    [ testCase "Empty Query" $
-        Lib2.parseQuery "" @?= Left "Error, command doesn't match any known query.",
-      testCase "Parsing case 1 - give a better name" $
-        Lib2.parseChangeTempoMelody "changeTempoMelody 1 +3" @?= Right (Lib2.ChangeTempoMelody 1 (Lib2.SmallInteger Lib2.Plus 3), ""),
-      testCase "Creating melody" $
-        Lib2.parseCreateMelody "createMelody 1 F2F4F8(A2((G16))B4) stop"
-          @?= Right
-            ( Lib2.CreateMelody
-                1
-                ( Lib2.CompoundMelody
-                    [ Lib2.SingleNote (Lib2.Note Lib2.F Lib2.Half),
-                      Lib2.SingleNote (Lib2.Note Lib2.F Lib2.Quarter),
-                      Lib2.SingleNote (Lib2.Note Lib2.F Lib2.Eighth),
-                      Lib2.CompoundMelody
-                        [ Lib2.SingleNote (Lib2.Note Lib2.A Lib2.Half),
-                          Lib2.CompoundMelody [Lib2.CompoundMelody [Lib2.SingleNote (Lib2.Note Lib2.G Lib2.Sixteenth)]],
-                          Lib2.SingleNote (Lib2.Note Lib2.B Lib2.Quarter)
-                        ]
-                    ]
-                ),
-              ""
-            )
+    "Unit Tests"
+    [ testCase "Parsing empty string fails" $
+        Lib2.parseQuery "" @?= Left "Error! could not parse that",
+      testCase "rendering works?" $
+        Lib3.renderStatements (Lib3.Batch [Lib2.CreateMelody 1 (Lib2.CompoundMelody [Lib2.SingleNote (Lib2.Note Lib2.A Lib2.Whole)])]) @?= "BEGIN createMelody 1 A1 stop; END",
+      testCase "parsing works?" $
+        Lib3.parseStatements "BEGIN createMelody 1 A1 stop END" @?= Right (Lib3.Batch [Lib2.CreateMelody 1 (Lib2.CompoundMelody [Lib2.SingleNote (Lib2.Note Lib2.A Lib2.Whole)])], ""),
+      testCase "Batch rendering works?" $
+        Lib3.renderStatements (Lib3.Batch [Lib2.CreateMelody 1 (Lib2.SingleNote (Lib2.Note Lib2.A Lib2.Whole)), Lib2.CreateMelody 2 (Lib2.SingleNote (Lib2.Note Lib2.B Lib2.Half))]) @?= "BEGIN createMelody 1 A1 stop; createMelody 2 B2 stop; END"
     ]
